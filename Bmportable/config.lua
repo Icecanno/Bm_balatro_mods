24d4f53c29bba9f2af888ed028b21c65d7501b13
BaiMao <149583568+Icecanno@users.noreply.github.com>
2026-04-02 12:20:54 +0800

    Update config.lua

--- Bmportable/config.lua
+++ Bmportable/config.lua
@@ -1,9 +1,11 @@
 return {
-    instant_planet = false,
+    misprint_display = false,
+    flip_sort = false,
     predict_random = false,
     predict_random_bonus = false,
     flash_load = false,
     manual_save = false,
+    score_parse = false,
     score_display = false,
     reduce_animation = false,
 }
