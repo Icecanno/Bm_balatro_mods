680110b40bdf7260ab0f54a5cc1a4c42f5c9687f
BaiMao <149583568+Icecanno@users.noreply.github.com>
2026-07-04 12:29:04 +0800

    Update en-us.lua

--- Bmportable/localization/en-us.lua
+++ Bmportable/localization/en-us.lua
@@ -1,6 +1,85 @@
 return {
     descriptions = {
         Other={
+            bmportable_misprint_display = {
+                name = 'Misprint Display',
+                text = {
+                    'Show the rank or suit of the',
+                    'next Misprint result in deck preview',
+                },
+            },
+            bmportable_deck_order = {
+                name = 'Deck Order',
+                text = {
+                    'Add a tab in deck view to inspect',
+                    'cards in actual draw order',
+                },
+            },
+            bmportable_flip_sort = {
+                name = 'Flip Sort',
+                text = {
+                    'Flip facedown hand cards and',
+                    'Jokers before sorting',
+                },
+            },
+            bmportable_predict_random = {
+                name = 'Predict Random',
+                text = {
+                    'Append the current random',
+                    'result preview to card tooltips',
+                },
+            },
+            bmportable_predict_random_bonus = {
+                name = 'Predict Bonus',
+                text = {
+                    'Extend random previews to more',
+                    'Jokers, packs, tags and Boss Blinds',
+                    'Requires Predict Random',
+                },
+            },
+            bmportable_flash_load = {
+                name = 'Flash Load',
+                text = {
+                    'Rebuild the run and jump back',
+                    'into gameplay when loading',
+                    'to reduce transition time',
+                },
+            },
+            bmportable_manual_save = {
+                name = 'Manual Save',
+                text = {
+                    'Add manual save and load',
+                    'buttons to the in-run menu',
+                },
+            },
+            bmportable_score_parse = {
+                name = 'Score Parse',
+                text = {
+                    'Simulate the current score',
+                    'during hand selection',
+                },
+            },
+            bmportable_score_display = {
+                name = 'Score Display',
+                text = {
+                    "Show a preview of this hand's",
+                    'score result after resolution',
+                },
+            },
+            bmportable_reduce_animation = {
+                name = 'Reduce Animation',
+                text = {
+                    'Reduce animation effects caused by',
+                    'money, chips, hand levels and scoring',
+                },
+            },
+            bmportable_zoom_out = {
+                name = 'Zoom Out',
+                text = {
+                    'Scale the overall view down',
+                    'to show more of the table',
+                },
+            },
             Preview={
                 name = 'Result',
                 text = {
