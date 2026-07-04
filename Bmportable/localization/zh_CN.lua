65191c4570ee7f84a0f3ac8fedd9d84d1ff1b87f
BaiMao <149583568+Icecanno@users.noreply.github.com>
2026-07-04 12:28:46 +0800

    Update zh_CN.lua

--- Bmportable/localization/zh_CN.lua
+++ Bmportable/localization/zh_CN.lua
@@ -1,6 +1,85 @@
 return {
     descriptions = {
         Other={
+            bmportable_misprint_display = {
+                name = '错印显示',
+                text = {
+                    '在牌堆预览里显示',
+                    '下一张错印的点数或花色',
+                },
+            },
+            bmportable_deck_order = {
+                name = '卡牌顺序',
+                text = {
+                    '在牌组查看页新增',
+                    '按实际抽牌顺序查看的标签页',
+                },
+            },
+            bmportable_flip_sort = {
+                name = '翻转理牌',
+                text = {
+                    '理牌前自动翻开背面手牌',
+                    '或小丑栏上的小丑',
+                },
+            },
+            bmportable_predict_random = {
+                name = '随机预测',
+                text = {
+                    '在卡牌说明里追加',
+                    '当前随机结果预览',
+                },
+            },
+            bmportable_predict_random_bonus = {
+                name = '预测增强',
+                text = {
+                    '扩展随机预览到更多小丑、',
+                    '补充包、标签、Boss盲注等',
+                    '需先开启“随机预测”',
+                },
+            },
+            bmportable_flash_load = {
+                name = '闪速加载',
+                text = {
+                    '快速读档时直接重建进入',
+                    '游玩界面，减少过场动画',
+                },
+            },
+            bmportable_manual_save = {
+                name = '手动存档',
+                text = {
+                    '在局内菜单加入',
+                    '手动存档或读档按钮',
+                },
+            },
+            bmportable_score_parse = {
+                name = '分数解析',
+                text = {
+                    '选牌阶段实时推演',
+                    '当前得分',
+                },
+            },
+            bmportable_score_display = {
+                name = '分数显示',
+                text = {
+                    '在实际结算后显示',
+                    '本次出牌的分数结果预览',
+                },
+            },
+            bmportable_reduce_animation = {
+                name = '削减动画',
+                text = {
+                    '尽量减少金币、筹码、',
+                    '牌型升级、计分等数值变动',
+                    '带来的动画特效',
+                },
+            },
+            bmportable_zoom_out = {
+                name = '相机拉远',
+                text = {
+                    '缩小整体视图，',
+                    '给桌面更多可视空间',
+                },
+            },
             Preview={
                 name = '结果',
                 text = {
